118ea45341a1060706275bc8cdac9ba3a26fe073
padi-dev-manhtd <manhtd@paditech.com>
2024-08-04 18:16:04 +0700

    profile user page

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -6,6 +6,8 @@ export function getUserDataSelect(loggedInUserId: string) {
     username: true,
     displayName: true,
     avatarUrl: true,
+    bio: true,
+    createAt: true,
     followers: {
       where: {
         followerId: loggedInUserId,
@@ -16,11 +18,13 @@ export function getUserDataSelect(loggedInUserId: string) {
     },
     _count: {
       select: {
+        posts: true,
         followers: true,
       },
     },
   } satisfies Prisma.UserSelect;
 }
+
 export type UserData = Prisma.UserGetPayload<{
   select: ReturnType<typeof getUserDataSelect>;
 }>;
